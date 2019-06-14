c2989c628fc2fd6cf00818fdfdb3541234954fa4
alanndz <alanmahmud0@gmail.com>
2019-06-14 16:45:59 +0000

    adapt to KomodOS Rom
    Signed-off-by: alanndz <alanmahmud0@gmail.com>

--- src/com/android/launcher3/Workspace.java
+++ src/com/android/launcher3/Workspace.java
@@ -57,7 +57,7 @@ import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.Toast;
 
-import com.android.internal.util.bootleggers.BootlegUtils;
+import com.android.internal.util.komodo.KomodoUtils;
 
 import com.android.launcher3.Launcher.LauncherOverlay;
 import com.android.launcher3.LauncherAppWidgetHost.ProviderChangedListener;
@@ -329,11 +329,11 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
                 break;
             // Sleep
             case 1:
-                BootlegUtils.switchScreenOff(getContext());
+                KomodoUtils.switchScreenOff(getContext());
                 break;
             // Flashlight
             case 2:
-                BootlegUtils.toggleCameraFlash();
+                KomodoUtils.toggleCameraFlash();
                 break;
         }
     }
